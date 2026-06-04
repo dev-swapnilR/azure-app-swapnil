3bb4d21a97e22a282358b93bdc5cc9fc87e974ae
Swapnil Rajurkar <swapnil.rajurkar@zohomail.in>
2026-06-04 08:03:04 +0530

    Added EF Core And Azure SQL DataBase Connection

---
+++ azure-app-swapnil/Migrations/20260604015611_RenameDaeOfBirthToDateOfBirth.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace azure_app_swapnil.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameDaeOfBirthToDateOfBirth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DaeOfBirth",
+                table: "Persons",
+                newName: "DateOfBirth");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Persons",
+                newName: "DaeOfBirth");
+        }
+    }
+}
